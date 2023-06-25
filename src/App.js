4370dbae4b31b89e82ec412cdc7b9cbe7aecdf30
bisrats <bisrat.shebeshe@gmail.com>
2023-06-25 17:21:45 -0400

    installed tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
-import img from './frontPage.jpg';
+import Darkimg from './darkBackground.jpg';
+import Chatlogo from '/home/bisrat/New_Folder/my-app/src/logo.svg';
 import './App.css';
 
 function HomePage() {
   return (
     <div className="App-body">
       <div className="content">
-      <h1 className="title">My First Component</h1>
+      <h1 className="underline ">My First Component</h1>
         <span>This is my first react page!</span>
       <h2>Voila!</h2>
       </div>
     </div>
-
-//     <div className="App">
-//       <header className="App-header">
-//         <p className="App-body">
-//         <img src={img} className="App-logo" alt="logo" />
-//           This is my first react page!
-//           <h2>Voila!</h2>
-//         Are you ready to  
-//         <a
-//           className="App-link"
-//           href="/home/bisrat/New_Folder/my-app/src/index.html"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//             Learn React?
-//         </a>
-//         </p>
-//       </header>
-//     </div>
   );
 }
 
-export default HomePage;
+function DarkCard(){
+return <img src={Darkimg } alt="Dark Card" className="blackCard"/>
+}
+
+function Chat(){
+  return (
+    <div class="p-6 max-w-sm mx-auto bg-grey rounded-xl shadow-lg shadow-black flex items-center space-x-4">
+    <div class="shrink-0">
+    <img class="h-12 w-12" src={Chatlogo} alt="ChitChat Logo"/>
+    </div>
+    <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div>
+  );
+}
+
+function End(){
+  return(<div class="">End of Page</div>)
+}
+export default function HomePagewithDarkCard(){
+  return(
+    <section>
+      <HomePage/>
+      <DarkCard/>
+      <Chat/>
+      <End/>
+    </section>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "/home/bisrat/New_Folder/my-app/src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
