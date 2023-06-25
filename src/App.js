28048d8e1ec233fff956104ac571e20671e8895a
bisrats <bisrat.shebeshe@gmail.com>
2023-06-25 14:55:36 -0400

    changed the default export name, page content and background

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import logo from './logo.svg';
+import img from './frontPage.jpg';
 import './App.css';
 
-function App() {
+function HomePage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my first react page!
-          <h2>Voila!</h2>
-        </p>
-        <p>Are you ready to  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-            Learn React?
-        </a>
-        </p>
-      </header>
+    <div className="App-body">
+      <div className="content">
+      <h1 className="title">My First Component</h1>
+        <span>This is my first react page!</span>
+      <h2>Voila!</h2>
+      </div>
     </div>
+
+//     <div className="App">
+//       <header className="App-header">
+//         <p className="App-body">
+//         <img src={img} className="App-logo" alt="logo" />
+//           This is my first react page!
+//           <h2>Voila!</h2>
+//         Are you ready to  
+//         <a
+//           className="App-link"
+//           href="/home/bisrat/New_Folder/my-app/src/index.html"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//             Learn React?
+//         </a>
+//         </p>
+//       </header>
+//     </div>
   );
 }
 
-export default App;
+export default HomePage;
